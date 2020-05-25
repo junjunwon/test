4f3bd0c45f3c332d3de87a75230902c0c948fa3b
junjunwon <45681336+junjunwon@users.noreply.github.com>
2020-05-25 14:58:54 +0900

    Update test.java

--- git_test/src/git_test/test.java
+++ git_test/src/git_test/test.java
@@ -4,5 +4,10 @@ public class test {
 	public static void main(String[]args){
 		
 		System.out.println("what the fuck!");
+		for(int i=0; i<4; i++){
+			if(i==0 || i==4){
+				System.out.println(i);
+			}
+		}
 	}
 }
