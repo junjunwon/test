38b98eabf2d9a66a9ee821156be9c67895c0066f
junjunwon <wnsgh1993@gmail.com>
2020-05-25 17:04:53 +0900

    only for testing and then will delete

--- Git/src/Git.java
+++ Git/src/Git.java
@@ -1,4 +1,17 @@
-
-public class Git {
+import java.io.*;
+public class Git {	
 	
+	//====================================
+	//change the code for testing git connection
+	public static void main(String[]args){
+		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		for(int i=0; i<10 ;i++){
+			if(i==0 || i==4){
+				continue;
+			}
+			System.out.println(i);
+		}
+	}
 }
