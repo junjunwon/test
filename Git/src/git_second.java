cdb50003abdd7b9bb38051e9d743322e2ebd33f6
junjunwon <wnsgh1993@gmail.com>
2020-05-25 16:01:44 +0900

    modify and create java class file and push it on git.

---
+++ Git/src/git_second.java
@@ -0,0 +1,16 @@
+
+public class git_second {
+	public static void main(String[]args){
+		int map[][]=new int[4][4];
+		int count=0;
+		for(int i=0; i<map.length; i++){
+			for(int j=0; j<map.length; j++){
+				map[i][j]=count++;
+			}
+		}
+		
+		int dxy[][]=new int[2][4];
+		
+		
+	}
+}
