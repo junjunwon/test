295ef0d4e2d933480c0dd6b4b89f9515b18132fc
junjunwon <wnsgh1993@gmail.com>
2020-05-25 09:27:05 +0900

    second commit

---
+++ test2.cpp
@@ -0,0 +1,18 @@
+//
+//  test2.cpp
+//  junhoTest
+//
+//  Created by 원준호 on 2020/05/25.
+//  Copyright © 2020 wonjunho. All rights reserved.
+//
+
+#include "test2.hpp"
+#include <iostream>
+using namespace std;
+
+int main(void){
+    
+    cout<<"test2";
+    return 0;
+    
+}
