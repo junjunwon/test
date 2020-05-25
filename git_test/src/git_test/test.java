853d5e3884e2b7bef3211f21cb9ca11372a09a04
junjunwon <wnsgh1993@gmail.com>
2020-05-25 17:05:54 +0900

    only for testing 2

---
+++ git_test/src/git_test/test.java
@@ -0,0 +1,18 @@
+package git_test;dddddㅇㅇㅇㅇcddddddddddd
+
+public class test {
+	public static void main(String[]args){
+
+		System.out.println("what the fuck!");
+		for(int i=0; i<4; i++){
+			if(i==0 || i==4){
+				System.out.println(i);
+			}
+		}ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇdfdfdf
+
+		HashMap<Integer, Integer> map=new HashMap<>():
+
+		map.put(0, 0);
+		map.put(1,2);
+	}
+}
